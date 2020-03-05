f17251f794d513f3504fdfd186a4d304e7449fe4
rmamchyk <roman.mamchyk@gmail.com>
2020-03-05 20:54:50 +0200

    added simple bars using divs

--- D3 Scales/app.js
+++ D3 Scales/app.js
@@ -1,1 +1,12 @@
 var data = [10, 20, 30, 40, 50];
+
+d3.select('#chart')
+    .selectAll('div')
+    .data(data)
+    .enter()
+    .append('div')
+    .attr('class', 'bar')
+    .style('height', d => {
+        const height = d * 3;
+        return height + 'px'
+    });
