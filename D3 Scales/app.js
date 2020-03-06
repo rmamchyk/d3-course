def54c173da8a1afdcd0a4dc34aa0c1308e69499
rmamchyk <roman.mamchyk@gmail.com>
2020-03-06 17:27:37 +0200

    changed html bars to svg

--- D3 Scales/app.js
+++ D3 Scales/app.js
@@ -1,16 +1,51 @@
 var data = [];
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 5; i++) {
     const num = Math.floor(d3.randomUniform(1, 50)());
     data.push(num);
 }
 
-d3.select('#chart')
-    .selectAll('div')
+const CHART_WIDTH = 800;
+const CHART_HEIGHT = 400;
+const BAR_PADDING = 5;
+
+// create SVG element
+const svg = d3.select('#chart')
+    .append('svg')
+    .attr('width', CHART_WIDTH)
+    .attr('height', CHART_HEIGHT);
+
+// bind data and create bars
+svg.selectAll('rect')
     .data(data)
     .enter()
-    .append('div')
-    .attr('class', 'bar')
-    .style('height', d => {
-        const height = d * 3;
-        return height + 'px'
-    });
+    .append('rect')
+    .attr('x', (d, i) => {
+        return i * (CHART_WIDTH / data.length);
+    })
+    .attr('y', d => {
+        return CHART_HEIGHT - d * 5;
+    })
+    .attr('width', CHART_WIDTH / data.length - BAR_PADDING)
+    .attr('height', d => {
+        return d * 5;
+    })
+    .style('fill', '#7ED26D');
+
+// create labels
+svg.selectAll('text')
+    .data(data)
+    .enter()
+    .append('text')
+    .text(d => {
+        return d;
+    })
+    .attr('x', (d, i) => {
+        return i * (CHART_WIDTH / data.length) +
+                    (CHART_WIDTH / data.length - BAR_PADDING) / 2;
+    })
+    .attr('y', d => {
+        return CHART_HEIGHT - d * 5 + 15;
+    })
+    .attr('font-size', 14)
+    .attr('fill', '#fff')
+    .attr('text-anchor', 'middle');
